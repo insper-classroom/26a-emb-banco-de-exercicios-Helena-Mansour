fdd6a03c809c081f3537cf0c83280abc39ec04f2
Helena-Mansour <helenasm@al.insper.edu.br>
2026-03-24 19:04:54 -0300

    .

--- pisca-led-tempo-botao/main.c
+++ pisca-led-tempo-botao/main.c
@@ -4,118 +4,88 @@
 #include "hardware/gpio.h"
 #include "hardware/timer.h"
 #include "hardware/irq.h"
+//teste
+const int LED_PIN_Y = 5;
+const int BTN_PIN_G = 28;
 
-const int BTN_PIN = 21;
-const int LED_PIN = 16;
-
-volatile bool pisca = false;
-
-//botao precisonado
-volatile bool btn_press = false;
-
-//quando comecou a apertar
-volatile uint32_t t_inicial = 0;
-
-//quanto tempo segurou
-volatile uint32_t t_pressionado = 500;
-
-//botao solto
-volatile bool atualiza = false;
-
-//hora de parar
-volatile bool parar = false;
-
-//led rodando
-volatile bool ativo = false; 
+volatile int btn_f = 0;
+volatile int g_timer_g = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
-    if(events == 0x4){ //botao apertado
-        btn_press = true;
-
-        //horario atual
-        t_inicial = to_ms_since_boot(get_absolute_time()); 
-    }
-    if(events == 0x8){ //botao solto
-        uint32_t t_final = to_ms_since_boot(get_absolute_time());
-
-        //horario final menos inicial
-        t_pressionado = t_final - t_inicial;
-        btn_press = false;
-        atualiza = true;
+    if (events & 0x4) {
+        btn_f = 1;
+    } else if (events & 0x8) { 
+        btn_f = 2;
     }
 }
 
-bool timer_callback(repeating_timer_t *rt) {
-     //so pisca se nao tiver pressionado e se esta ativo
-    if(!btn_press && ativo){
-        pisca = true;
-    }
+bool timer_g_callback(repeating_timer_t *rt) {
+    g_timer_g = 1;
     return true;
-}
-
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    parar = true;
-    return 0;
 }
 
 int main() {
     stdio_init_all();
-
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_put(LED_PIN, 0);
     
-    gpio_init(BTN_PIN);
-    gpio_set_dir(BTN_PIN, GPIO_IN);
-    gpio_pull_up(BTN_PIN);
-    gpio_set_irq_enabled_with_callback(
-        BTN_PIN,
-        GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE,
-        true,
-        &btn_callback
-    );
+    gpio_init(BTN_PIN_G);
+    gpio_set_dir(BTN_PIN_G, GPIO_IN);
+    gpio_pull_up(BTN_PIN_G);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_G, 0x4 | 0x8, true, &btn_callback);
 
-    repeating_timer_t timer;
+    gpio_init(LED_PIN_Y);
+    gpio_set_dir(LED_PIN_Y, GPIO_OUT);
 
-    //timer pisca a cada 200ms
-    add_repeating_timer_ms(200, timer_callback, NULL, &timer);
+    struct repeating_timer timer;
+    bool timer_ativo = false;
+    uint32_t time_start = 0;
+    int led_g = 0;
+    uint32_t time_press = 0;
 
-    bool led_estado = false;
+    while (1) {
+        uint32_t agora = to_ms_since_boot(get_absolute_time());
 
-    while (true) {
+        if (btn_f == 1) {
+            btn_f = 0;
 
-        // enquanto pressiona o LED apagado
-        if(btn_press){ 
-            gpio_put(LED_PIN,0);
-        }
+            if (agora - time_start > 50) {
+                gpio_put(LED_PIN_Y, 0);
+                led_g = 0;
 
-        // piscar LED (só se ativo)
-        if(pisca && !btn_press && ativo){
-            led_estado = !led_estado;
-            gpio_put(LED_PIN, led_estado);
-            pisca = false;
-        }
-
-        // quando solta botão reinicia inicia ciclo
-        if(atualiza){
-            //garante q o tempo minimo seja 50 ms
-            if(t_pressionado < 50){
-                t_pressionado = 50;
+                if (timer_ativo) {
+                    cancel_repeating_timer(&timer);
+                    timer_ativo = false;
+                }
+                
+                time_start = agora;
             }
+            
+        } else if (btn_f == 2) {
+            btn_f = 0;
 
-            ativo = true; // ativa o sistema
-
-            //led pisca por t_pressionado de tempo
-            add_alarm_in_ms(t_pressionado, alarm_callback, NULL, false);
-
-            atualiza = false;
+            time_press = agora - time_start;
+            
+            if (time_press > 50) {
+                time_start = agora;
+                
+                add_repeating_timer_ms(250, timer_g_callback, NULL, &timer);
+                timer_ativo = true;
+            }
         }
 
-        // parar quando tempo acabar
-        if(parar){
-            parar = false;
-            ativo = false;
-            gpio_put(LED_PIN, 0);
+        if (g_timer_g == 1) {
+            g_timer_g = 0;
+
+            if (timer_ativo) {
+                if (agora - time_start >= time_press) {
+                    timer_ativo = false;
+                    led_g = 0;
+                    gpio_put(LED_PIN_Y, led_g);
+                    cancel_repeating_timer(&timer);
+                } else {
+                    led_g = !led_g;
+                    gpio_put(LED_PIN_Y, led_g);
+                }
+            }
         }
     }
 
