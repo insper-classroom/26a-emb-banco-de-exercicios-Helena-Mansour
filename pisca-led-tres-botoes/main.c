c810c19f56ada50a27e029f9b35e9206d44bfd05
Helena-Mansour <helenasm@al.insper.edu.br>
2026-04-24 10:18:09 -0300

    pisca led tres botoes

--- pisca-led-tres-botoes/main.c
+++ pisca-led-tres-botoes/main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 #include "hardware/adc.h"
@@ -6,53 +5,41 @@
 #include "pico/stdlib.h"
 
 const int BTN_PIN_Y = 28;
-const int BTN_PIN_B = 22;
 const int BTN_PIN_G = 18;
+const int BTN_PIN_B = 22;
 
 const int LED_PIN_G = 14;
 const int LED_PIN_Y = 6;
 const int LED_PIN_B = 10;
 
 
-//botao amarelo precionado
 volatile bool btn_press_g= false;
 volatile bool btn_press_y= false;
 volatile bool btn_press_b= false;
 
 
 
-
 void btn_callback(uint gpio, uint32_t events) {
-    if(events == 0x4){ //se o botao é precionado
+    if(events == 0x4){ 
+        if(gpio == BTN_PIN_Y){
+            btn_press_y = !btn_press_y; 
+        }
         if(gpio == BTN_PIN_G){
             btn_press_g = true; 
         }
-        if(gpio == BTN_PIN_Y){
-            btn_press_y = !btn_press_y; //marca a flag
-        }
     }
     if(events == 0x8){ 
+        if(gpio == BTN_PIN_B){
+            btn_press_b = !btn_press_b; 
+        }
         if(gpio == BTN_PIN_G){
             btn_press_g = false; 
         }
-        if(gpio == BTN_PIN_B){
-            btn_press_b = !btn_press_b; //marca a flag
-        }
     }
 }
 
-bool timer_y_callback(repeating_timer_t *rt) {
-    return true;
 
-}
 
-bool timer_g_callback(repeating_timer_t *rt) {
-    return true;
-}
-
-bool timer_b_callback(repeating_timer_t *rt) {
-    return true;
-}
 
 
 
@@ -77,7 +64,6 @@ int main() {
     gpio_set_irq_enabled_with_callback(
     BTN_PIN_B, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
-
     gpio_init(LED_PIN_G);
     gpio_set_dir(LED_PIN_G, GPIO_OUT);
 
@@ -86,51 +72,48 @@ int main() {
 
     gpio_init(LED_PIN_B);
     gpio_set_dir(LED_PIN_B, GPIO_OUT);
+ 
+  
+  
+    bool led_estado= false;
 
   
-
-
-    bool led_estado = false;
-
-  
-    while (1) {
+    while (1) { 
+        
         led_estado = !led_estado;
-
-        if(btn_press_y){
-
-            gpio_put(LED_PIN_Y, led_estado);
-                   
-        }else{
-            gpio_put(LED_PIN_Y, 0);
-
+        
+        if(btn_press_g){
+            gpio_put(LED_PIN_G, led_estado);
+         
         }
 
-        if(btn_press_g){
-
-            gpio_put(LED_PIN_G, led_estado);
-                   
-        }else{
+        if(!btn_press_g){
             gpio_put(LED_PIN_G, 0);
+         
+        }
+     
 
+        if(btn_press_y){
+            gpio_put(LED_PIN_Y, led_estado);
+        }
+        if(!btn_press_y){
+            gpio_put(LED_PIN_Y, 0);
         }
 
         if(btn_press_b){
-
             gpio_put(LED_PIN_B, led_estado);
-                   
-        }else{
+            
+         }
+         if(!btn_press_b){
+    
             gpio_put(LED_PIN_B, 0);
-
-        }
+            
+         }
 
         sleep_ms(200);
+      
 
-  
-
-     
-        } 
+        
 
     }
-
-
-
+    }
