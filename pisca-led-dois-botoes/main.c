768d623ff4e8f7eaab43045b5f9b4321c6cc88b6
Helena-Mansour <helenasm@al.insper.edu.br>
2026-04-24 09:26:31 -0300

    pisca led dois botoes

--- pisca-led-dois-botoes/main.c
+++ pisca-led-dois-botoes/main.c
@@ -1,20 +1,24 @@
-
 #include <stdio.h>
 
 #include "hardware/adc.h"
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 
-const int BTN_PIN_Y = 19;
-const int BTN_PIN_G = 18;
+const int BTN_PIN_Y = 26;
+const int BTN_PIN_G = 28;
 
-const int LED_PIN_G = 16;
-const int LED_PIN_Y = 15;
+const int LED_PIN_G = 5;
+const int LED_PIN_Y = 9;
 
 
 volatile bool btn_press_g= false;
 volatile bool btn_press_y= false;
 
+volatile bool alarme_y= false;
+volatile bool alarme_g= false;
+
+volatile bool pisca_g = false;
+volatile bool pisca_y = false;
 
 void btn_callback(uint gpio, uint32_t events) {
     if(events == 0x4){ //se o botao é precionado
@@ -27,6 +31,29 @@ void btn_callback(uint gpio, uint32_t events) {
     }
 }
 
+bool timer_y_callback(repeating_timer_t *rt) {
+    pisca_y = true;
+    return true;
+
+}
+
+bool timer_g_callback(repeating_timer_t *rt) {
+    pisca_g = true;
+    return true;
+}
+
+
+int64_t alarm_callback_g(alarm_id_t id, void *user_data) {
+    alarme_g = true;
+    return 0;
+
+}
+
+int64_t alarm_callback_y(alarm_id_t id, void *user_data) {
+    alarme_y = true;
+    return 0;
+
+}
 
 
 
@@ -67,7 +94,26 @@ int main() {
 
             gpio_put(LED_PIN_G, 1);
 
+            add_repeating_timer_ms(200, timer_g_callback, NULL, &time_g);
+            add_alarm_in_ms(1000, alarm_callback_g, NULL, false);
          
+        }
+        if (alarme_g){
+            alarme_g = false;
+            gpio_put(LED_PIN_G, 0);
+
+            cancel_repeating_timer(&time_g);
+
+            //alarme_y = false;
+            gpio_put(LED_PIN_Y, 0);
+
+            cancel_repeating_timer(&time_y);
+        
+        }
+        if(pisca_g){
+            pisca_g = false;
+            led_estado_g = !led_estado_g;
+            gpio_put(LED_PIN_G, led_estado_g);
         }
 
         if(btn_press_y){
@@ -75,13 +121,29 @@ int main() {
             btn_press_y = false;
             gpio_put(LED_PIN_Y, 1);
 
+            add_repeating_timer_ms(500, timer_y_callback, NULL, &time_y);
+            add_alarm_in_ms(2000, alarm_callback_y, NULL, false);
          
+        }
+        if (alarme_y){
+            alarme_y = false;
+            gpio_put(LED_PIN_Y, 0);
+
+            cancel_repeating_timer(&time_y);
+
+            //alarme_g = false;
+            gpio_put(LED_PIN_G, 0);
+
+            cancel_repeating_timer(&time_g);
+        
+        }
+        if(pisca_y){
+            pisca_y = false;
+            led_estado_y = !led_estado_y;
+            gpio_put(LED_PIN_Y, led_estado_y);
         }
 
         
 
     }
     }
-
-
-
